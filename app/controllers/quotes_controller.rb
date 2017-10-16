4062aeb76a3312a1246535fac479741bd0ade682
Rob Black <rtblack628@gmail.com>
2017-10-16 04:41:18 +0000

    Added validation for character lengths of contributions.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,20 +1,23 @@
 class QuotesController < ApplicationController
-    def index
-        @quote = Quote.order("RANDOM()").first
-    end
+	def index
+			@quote = Quote.order("RANDOM()").first
+	end
 
-    def new 
-        @quote = Quote.new
-    end
+	def new 
+		@quote = Quote.new
+	end
 
-    def create 
-        Quote.create(quote_params)
-        redirect_to root_path
-    end
+	def create 
+		@quote = Quote.create(quote_params)
+  	if @quote.invalid?
+    	flash[:error] = "<strong>Error!</strong> Please make sure that you've completed the required fields. Your quote must be 250 characters or less, and the author should be 100 characters or less."
+		end         
+		redirect_to root_path
+	end
 
-    private
+	private
 
-    def quote_params 
-        params.require(:quote).permit(:saying, :author)
-    end
+	def quote_params 
+			params.require(:quote).permit(:saying, :author)
+	end
 end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,4 @@
 class Quote < ApplicationRecord
+  validates :saying, presence: true, length: { maximum: 250, minimum: 2 }
+  validates :author, presence: true, length: { maximum: 100, minimum: 1 }
 end
